78aa7be35110eb6bf72d12d256c7456928327b71
JoyNjihia <nyamburanjihia@gmail.com>
2025-04-13 10:42:29 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,20 @@
 
+function changeText() {
+  const intro = document.getElementById('intro');
+  intro.textContent = "Yay! The paragraph has been updated using JavaScript!";
+  intro.classList.toggle("highlight");
+}
+
+function toggleElement() {
+  const area = document.getElementById('dynamic-area');
+  const existing = document.getElementById('new-element');
+
+  if (existing) {
+    area.removeChild(existing);
+  } else {
+    const newElement = document.createElement('p');
+    newElement.textContent = "This new paragraph was added with JavaScript!";
+    newElement.id = "new-element";
+    area.appendChild(newElement);
+  }
+}
